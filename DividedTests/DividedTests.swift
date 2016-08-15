f13adbb454bc3c117e87b56d9e0636db2f3b508f
Dustin Morando <dustinmorando@me.com>
2016-08-15 14:09:26 -0400

    Initial Commit

---
+++ DividedTests/DividedTests.swift
@@ -0,0 +1,36 @@
+//
+//  DividedTests.swift
+//  DividedTests
+//
+//  Created by Dustin Morando on 8/15/16.
+//  Copyright © 2016 Dustin Morando. All rights reserved.
+//
+
+import XCTest
+@testable import Divided
+
+class DividedTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
